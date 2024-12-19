51ab75c88bde154d143a64dc6ddd11426c5ad188
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2024-12-19 15:57:01 +0100

    add Mui css

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
 import {AuthProvider} from "./contexts/AuthProvider.tsx";
 import ToastProvider from "./contexts/ToastProvider.tsx";
 import HouseholdProvider from "./contexts/HouseholdProvider.tsx";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {CssBaseline} from "@mui/material";
 
 const queryClient = new QueryClient()
 
@@ -17,6 +17,7 @@ createRoot(document.getElementById('root')!).render(
         <ToastProvider>
           <AuthProvider>
             <HouseholdProvider>
+              <CssBaseline/>
               <App/>
             </HouseholdProvider>
           </AuthProvider>
